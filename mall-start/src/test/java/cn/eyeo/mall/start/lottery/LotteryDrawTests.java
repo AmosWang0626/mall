7936de018e6b18fb9cc9fa7f04e961a67ad6e81d
amos.wang <1833063210@qq.com>
2023-03-27 23:38:16 +0800

    test: 补充日志

--- mall-start/src/test/java/cn/eyeo/mall/start/lottery/LotteryDrawTests.java
+++ mall-start/src/test/java/cn/eyeo/mall/start/lottery/LotteryDrawTests.java
@@ -19,7 +19,7 @@ public class LotteryDrawTests {
 
     @Test
     public void lottery() {
-        int[] array = {30, 40, 20};
+        int[] array = {30, 40, 20, 1};
 
         probability(array);
     }
@@ -29,24 +29,33 @@ public class LotteryDrawTests {
         if (sum > TOTAL_PROBABILITY) {
             throw new IllegalArgumentException("总概率不能>100");
         }
+
+        System.out.println("----------------------------------------------");
+        System.out.println("原始数据: " + Arrays.toString(source));
+
+        // 不满100%则补全
         int[] array = source;
         if (sum < TOTAL_PROBABILITY) {
             array = new int[source.length + 1];
             System.arraycopy(source, 0, array, 0, source.length);
             array[source.length] = TOTAL_PROBABILITY - sum;
         }
-        System.out.println(Arrays.toString(array));
+        System.out.println("补全后的数据: " + Arrays.toString(array));
 
+        // 计算概率，测试1000次
         Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 10000; i++) {
+            // 单次获取概率结果
             int index = getIndex(array);
-            System.out.printf("中奖啦! 概率: %d, Index: %d%n", array[index], index);
+            // System.out.printf("中奖啦! 概率: %d, Index: %d%n", array[index], index);
 
             Integer value = map.getOrDefault(array[index], 0);
             map.put(array[index], value + 1);
         }
 
-        System.out.println(map);
+        System.out.println("----------------------------------------------");
+        System.out.printf("看看结果吧! 概率: %s%n", map);
+        System.out.println("----------------------------------------------");
     }
 
     private static int getIndex(int[] array) {
